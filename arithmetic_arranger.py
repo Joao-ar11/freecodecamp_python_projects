3c07ca00b9b1319deacdf966e43c23d9bfe12bc1
João_A.11 <jarlpereira29@gmail.com>
2022-07-01 14:39:58 -0300

    It can sum and subtract now
    Added a codition for sum and subtraction

--- arithmetic_arranger.py
+++ arithmetic_arranger.py
@@ -1,5 +1,10 @@
 def arithmetic_arranger(problems):
     qnt = len(problems)
+    result = list()
     for problem in problems:
         element = problem.split()
-        print(element)
+        if element[1] == '+':
+            result.append(int(element[0]) + int(element[2]))
+        elif element[1] == '-':
+            result.append(int(element[0]) - int(element[2]))
+    print(result)
