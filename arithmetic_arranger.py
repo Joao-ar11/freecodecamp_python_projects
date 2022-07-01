ffc97da89f48fc2482c886bf9e00f29a31bf4b4e
João_A.11 <jarlpereira29@gmail.com>
2022-07-01 15:01:27 -0300

    The problems are formated now
    The equations are properly formatted

--- arithmetic_arranger.py
+++ arithmetic_arranger.py
@@ -1,10 +1,22 @@
 def arithmetic_arranger(problems):
     qnt = len(problems)
     result = list()
+    line1 = ''
+    line2 = ''
+    line3 = ''
+    line4 = ''
     for problem in problems:
         element = problem.split()
         if element[1] == '+':
-            result.append(int(element[0]) + int(element[2]))
+            result = int(element[0]) + int(element[2])
         elif element[1] == '-':
-            result.append(int(element[0]) - int(element[2]))
-    print(result)
+            result = int(element[0]) - int(element[2])
+        if len(element[0]) > len(element[2]):
+            length = len(element[0]) + 2
+        else:
+            length = len(element[2]) + 2
+        line1 += ' ' * (length - len(element[0])) + f'{element[0]}    '
+        line2 += f'{element[1]}' + ' ' * (length - len(element[2]) - 1) + f'{element[2]}    '
+        line3 += '-' * length + '    '
+        line4 += ' ' * (length - len(str(result))) + f'{result}    '
+    print(f'{line1}\n{line2}\n{line3}\n{line4}')
