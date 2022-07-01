e68decd4149a32875f26d24657ae64f08a013571
João_A.11 <jarlpereira29@gmail.com>
2022-07-01 15:26:10 -0300

    Error Messages +bugs!
    Added the error messages the project asked for, plus fixed a few type error that appeared.

--- arithmetic_arranger.py
+++ arithmetic_arranger.py
@@ -1,4 +1,7 @@
 def arithmetic_arranger(problems, answer=False):
+    if len(problems) > 5:
+        return 'Error: Too many problems.'
+       
     qnt = len(problems)
     result = list()
     line1 = ''
@@ -7,16 +10,25 @@ def arithmetic_arranger(problems, answer=False):
     line4 = ''
     for problem in problems:
         element = problem.split()
-        if element[1] == '+':
-            result = int(element[0]) + int(element[2])
-        elif element[1] == '-':
-            result = int(element[0]) - int(element[2])
+        if len(element[0]) > 4 or len(element[2]) > 4:
+            return 'Error: Numbers cannot be more than four digits.'
         if len(element[0]) > len(element[2]):
             length = len(element[0]) + 2
         else:
             length = len(element[2]) + 2
-        line1 += ' ' * (length - len(element[0])) + f'{element[0]}    '
-        line2 += f'{element[1]}' + ' ' * (length - len(element[2]) - 1) + f'{element[2]}    '
+        try:
+            element[0] = int(element[0])
+            element[2] = int(element[2])
+        except:
+            return 'Error: Numbers must only contain digits.'
+        if element[1] == '+':
+            result = element[0] + element[2]
+        elif element[1] == '-':
+            result = element[0] - element[2]
+        else:
+            return "Error: Operator must be '+' or '-'."
+        line1 += ' ' * (length - len(str(element[0]))) + f'{element[0]}    '
+        line2 += f'{element[1]}' + ' ' * (length - len(str(element[2])) - 1) + f'{element[2]}    '
         line3 += '-' * length + '    '
         line4 += ' ' * (length - len(str(result))) + f'{result}    '
     if answer:
