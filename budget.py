ec292a1a7df244630e53126b273609734979e71f
João_A.11 <jarlpereira29@gmail.com>
2022-07-11 13:47:33 -0300

    Removed the test part

--- budget.py
+++ budget.py
@@ -87,22 +87,3 @@ def create_spend_chart(categories):
         else:
             chart += ' '
     return chart
-            
-
-from unicodedata import category
-from unittest import main
-
-food = Category('Food')
-entertainment = Category('Entertainment')
-business = Category('Business')
-food.deposit(900, "deposit")
-entertainment.deposit(900, "deposit")
-business.deposit(900, "deposit")
-food.withdraw(105.55)
-entertainment.withdraw(33.40)
-business.withdraw(10.99)
-print(food)
-print(entertainment)
-print(business)
-print(create_spend_chart([business, food, entertainment]))
-
